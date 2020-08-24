da4103bc004ccde660cd45b1a00ca20ffbe04742
jtskidmore <johntoddskidmore@gmail.com>
2020-08-24 13:37:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(something, sm) {
+  let array = []
+  for (let i=0; i < something.length; i++) {
+    array.push(`Thank you, ${something[i]}, for the wonderful ${sm} gift!`);
+  }
+  return array
+}
+
+function countDown(number) {
+  let num = number
+  while (num >= 0) {
+    console.log(num)
+    num -= 1
+  }
+}
