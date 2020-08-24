563dc6cfd0321b582fb29aa1c80516b5c26f5f1d
reedanna <reedanna@mail.com>
2020-08-24 15:43:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thanks = [];
+    for (let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanks;
+};
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    };
+};
