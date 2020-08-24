67c769e5636be361b9c97f3d81518058511d9ae1
allenorton <norton.allene@gmail.com>
2020-08-24 11:34:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, reason) {
+    const rAy = []
+    for (let i = 0; i < names.length; i++) {
+        rAy.push(`Thank you, ${names[i]}, for the wonderful ${reason} gift!`);
+    }
+    return rAy
+}
+
+function countDown(number) {
+    let i = 0
+    while ( number > 0 ) {
+        console.log ( number );
+        number -= 1;
+    }
+    console.log( number )
+}
